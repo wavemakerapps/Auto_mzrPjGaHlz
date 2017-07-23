5a6a4033cdb691521cc302e501817540fe0aa401
root <root@61949b3d2f95>
2017-07-23 20:35:59 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_mzrPjGaHlz",
+  "homePage" : "Main",
+  "name" : "Auto_mzrPjGaHlz",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
